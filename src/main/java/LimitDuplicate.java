cd661c8fa358ddd796e35606a2268f5391a93452
Andrey <x137@yandex.ru>
2021-07-06 23:27:28 +0300

    Ex 13

--- src/main/java/LimitDuplicate.java
+++ src/main/java/LimitDuplicate.java
@@ -2,40 +2,28 @@ import java.util.*;
 
 public class LimitDuplicate {
     public static void main(String[] args) {
-        int[] arrays = {2, 2, 6, 6, 7, 1, 2, 4, 4, 4, 4, 7, 7};
-        int max = 2;
+        int[] elements = {2, 2, 6, 6, 7, 1, 2, 4, 4, 4, 4, 7, 7};
+        int maxOccurrences = 1;
 
-        for (int i = 0; i < result(arrays, max).length; i++) {
-            System.out.print(result(arrays, max)[i] + " ");
-
+        for (int i = 0; i < result(elements, maxOccurrences).length; i++) {
+            System.out.print(result(elements, maxOccurrences)[i] + " ");
         }
-
-
     }
 
-    public static int[] result(int[] arrays, int max) {
+    public static int[] result(int[] elements, int maxOccurrences) {
 
-        //int [] result = new int[arrays.length];
-//        int [] result = arrays.clone();
-//        result[1] =
+        if (maxOccurrences < 1)
+            return new int[]{};
 
+        Map<Integer, Integer> numOfElementsMap = new HashMap<>();
+        List<Integer> list = new ArrayList<>(elements.length);
 
-            if (max < 1)
-                return new int[]{};
-
-            Map<Integer, Integer> numOfElementsMap = new HashMap<>();
-            List<Integer> list = new ArrayList<>(arrays.length);
-
-            for (int element : arrays) {
-                if (numOfElementsMap.get(element) == null || numOfElementsMap.get(element) < max)
-                    list.add(element);
-                numOfElementsMap.merge(element, 1, Integer::sum);
-
-            }
-
-            return list.stream().mapToInt(Integer::intValue).toArray();
-
-
+        for (int element : elements) {
+            if (numOfElementsMap.get(element) == null || numOfElementsMap.get(element) < maxOccurrences)
+                list.add(element);
+            numOfElementsMap.merge(element, 1, Integer::sum);
+        }
+        return list.stream().mapToInt(Integer::intValue).toArray();
     }
 }
 
