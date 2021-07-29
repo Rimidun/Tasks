4f32b4abd1b4ffa52f432e884e2eb96624e58686
Andrey <x137@yandex.ru>
2021-07-29 23:31:03 +0300

    Ex 15

--- src/main/java/Mumbling.java
+++ src/main/java/Mumbling.java
@@ -1,14 +1,23 @@
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
 public class Mumbling {
     public static void main(String[] args) {
-        String s = "Wdarq";
-        accum(s);
+        String s = "Wdaawdawdawdrq";
+        System.out.println(accum(s));
 
     }
 
     public static String accum(String s){
-        int x = s.length();
-        System.out.println(x);
+        char[] chars = s.toUpperCase().toCharArray();
+        return IntStream
+                .range(0, chars.length)
+                .mapToObj(index -> chars[index] + Stream.generate(() -> String.valueOf(chars[index]))
+                        .limit(index)
+                        .collect(Collectors.joining())
+                        .toLowerCase()).collect(Collectors.joining("-"));
 
-        return s;
+
     }
 }
