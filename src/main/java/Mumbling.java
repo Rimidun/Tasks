269061be7aab63725c11dff7a8b3bc8198652c86
Andrey <x137@yandex.ru>
2021-07-29 23:10:17 +0300

    Ex 16

---
+++ src/main/java/Mumbling.java
@@ -0,0 +1,14 @@
+public class Mumbling {
+    public static void main(String[] args) {
+        String s = "Wdarq";
+        accum(s);
+
+    }
+
+    public static String accum(String s){
+        int x = s.length();
+        System.out.println(x);
+
+        return s;
+    }
+}
