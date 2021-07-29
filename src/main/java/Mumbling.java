5cb4bd531259a670cd0e75a55e17fc864c9b53d4
Andrey <x137@yandex.ru>
2021-07-29 23:33:07 +0300

    add ex 16 solution

--- src/main/java/Mumbling.java
+++ src/main/java/Mumbling.java
@@ -5,11 +5,13 @@ import java.util.stream.Stream;
 public class Mumbling {
     public static void main(String[] args) {
         String s = "Wdaawdawdawdrq";
-        System.out.println(accum(s));
+        System.out.println(accum1(s));
+
+        System.out.println(accum2(s));
 
     }
 
-    public static String accum(String s){
+    public static String accum1(String s){
         char[] chars = s.toUpperCase().toCharArray();
         return IntStream
                 .range(0, chars.length)
@@ -20,4 +22,18 @@ public class Mumbling {
 
 
     }
+
+    public static String accum2(String s) {
+        StringBuilder bldr = new StringBuilder();
+        int i = 0;
+        for(char c : s.toCharArray()) {
+            if(i > 0) bldr.append('-');
+            bldr.append(Character.toUpperCase(c));
+            for(int j = 0; j < i; j++) bldr.append(Character.toLowerCase(c));
+            i++;
+        }
+        return bldr.toString();
+    }
+
+
 }
