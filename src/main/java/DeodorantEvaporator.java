6483679c68bf158c49d8ea12901b771c63afcfd0
Andrey <x137@yandex.ru>
2021-08-22 23:39:51 +0300

    add ex 17 task

--- src/main/java/DeodorantEvaporator.java
+++ src/main/java/DeodorantEvaporator.java
@@ -1,5 +1,14 @@
 public class DeodorantEvaporator {
     public static void main(String[] args) {
+        double content = 10;
+        double evap_per_day = 10 ;
+        double threshold = 5;
 
+
+    }
+
+    public static int evaporator(double content, double evap_per_day, double threshold) {
+        // your code
+        return 1;
     }
 }
