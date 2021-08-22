90b0f50257ae9f8a699a4a49d462b6f30c9fb3de
Andrey <x137@yandex.ru>
2021-08-23 00:12:22 +0300

    add ex 17 task solution

--- src/main/java/DeodorantEvaporator.java
+++ src/main/java/DeodorantEvaporator.java
@@ -1,14 +1,25 @@
 public class DeodorantEvaporator {
     public static void main(String[] args) {
         double content = 10;
-        double evap_per_day = 10 ;
-        double threshold = 5;
+        double evap_per_day = 10;
+        double threshold = 10;
+
+
+        System.out.println(evaporator(content, evap_per_day, threshold));
 
 
     }
 
     public static int evaporator(double content, double evap_per_day, double threshold) {
-        // your code
-        return 1;
+
+
+        double lifeOfEvaporator = content * (threshold/100);
+        System.out.println(lifeOfEvaporator);
+        int day = 0;
+        while (content > lifeOfEvaporator) {
+            content = ((content * (evap_per_day/100) * (-1)) + content);
+            day++;
+        }
+        return day;
     }
 }
