db76b8729f386d316edd210d77bf5082af67f94a
Andrey <x137@yandex.ru>
2021-08-27 00:16:05 +0300

    add ex 22 task solution

--- src/main/java/RowWeights.java
+++ src/main/java/RowWeights.java
@@ -1,9 +1,26 @@
 public class RowWeights {
     public static void main(String[] args) {
-        final int [] weight = {80, 40 , 50 ,20 , 1};
+        final int[] weight = {99, 105, 33, 28, 29};
+        rowWeights(weight);
     }
 
-    public static int [] rowWeights(final int [] weight){
+    public static int[] rowWeights(final int[] weights) {
+
+        int first = 0;
+        int second = 0;
+
+        for (int i = 0; i < weights.length; i++) {
+            if (i % 2 == 0) {
+                first += weights[i];
+            } else {
+                second += weights[i];
+            }
+
+        }
+
+        System.out.println(first + " " + second);
+
+        return new int[]{first, second};
 
     }
 }
