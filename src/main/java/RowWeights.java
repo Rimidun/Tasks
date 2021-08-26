8e18ef3e53730bc6767a95e3e89a38aaa9505122
Andrey <x137@yandex.ru>
2021-08-26 23:47:12 +0300

    add ex 22 task

---
+++ src/main/java/RowWeights.java
@@ -0,0 +1,9 @@
+public class RowWeights {
+    public static void main(String[] args) {
+        final int [] weight = {80, 40 , 50 ,20 , 1};
+    }
+
+    public static int [] rowWeights(final int [] weight){
+
+    }
+}
