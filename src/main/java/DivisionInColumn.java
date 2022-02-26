eb76a1a0e36f720ddf33d0cf728382d93848cf07
Andrey <x137@yandex.ru>
2022-02-26 20:12:37 +0300

    ex 29

---
+++ src/main/java/DivisionInColumn.java
@@ -0,0 +1,12 @@
+public class DivisionInColumn {
+    public static void main(String[] args) {
+        int a = 100;
+        int b = 5;
+
+        System.out.println(result(a, b));
+    }
+
+    public static int result(int a, int b){
+        return a/b;
+    }
+}
