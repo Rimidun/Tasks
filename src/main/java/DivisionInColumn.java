380f9f7e312cc7add2e99a45f1cf3996399d9828
Andrey <x137@yandex.ru>
2022-02-27 23:40:51 +0300

    update task 29

--- src/main/java/DivisionInColumn.java
+++ src/main/java/DivisionInColumn.java
@@ -1,12 +1,31 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class DivisionInColumn {
     public static void main(String[] args) {
         int a = 100;
-        int b = 5;
+        int b = 3;
 
-        System.out.println(result(a, b));
+        System.out.println(div(a, b));
     }
 
-    public static int result(int a, int b){
-        return a/b;
+    public static long div(long dividend, long divisor) {
+        long quotient = 0;
+        int k = 0;
+        while (divisor <= dividend && divisor > 0) {
+            divisor <<= 1;
+            k++;
+        }
+        while (k-- > 0) {
+            divisor >>= 1;
+            if (divisor <= dividend) {
+                dividend -= divisor;
+                quotient = (quotient << 1) + 1;
+            }
+            else quotient <<= 1;
+        }
+        return quotient;
     }
+
+
 }
